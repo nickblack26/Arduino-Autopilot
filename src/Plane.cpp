f12a1e4600a2c1159ecaf4a0475bd33019801527
Nick Black <49686365+nickblack26@users.noreply.github.com>
2022-08-25 21:50:12 -0500

    Added getDesiredState and makeCorrection funcs

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -11,6 +11,13 @@ void Plane::moveServo(Servo *aServo, uint8_t value) {
     aServo->write(value); 
 }
 
+void Plane::makeCorrections() {
+    if (abs(roll_error )> 2.5 ) {
+       roll_servo_val_l = roll_servo_val_l + roll_error/1.5;
+       moveServo(&aileron_left, roll_servo_val_l);
+    }
+}
+
 float Plane::kalman(float u) { 
     static const float r = 40; 
     static const float h = 10; 
@@ -28,81 +35,37 @@ float Plane::kalman(float u) {
 }
 
 float Plane::filter(float &oldValue, float &currentValue) {
-    return 0.95 * oldValue + 0.05 * currentValue;
+    return 0.8 * oldValue + 0.2 * currentValue;
 }
 
 void Plane::getIMUdata() {
     imu::Vector<3> gyr = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
     imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
     imu::Vector<3> mag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-    // thetaM=-atan2(acc.x()/9.8,acc.z()/9.8)/2/3.141592654*360;
-    // phiM=-atan2(acc.y()/9.8,acc.z()/9.8)/2/3.141592654*360;
-    // phiFnew=.95*phiFold+.05*phiM;
-    // dt=(millis()-millisOld)/1000.;
-    // millisOld=millis();
-    // theta=(theta+gyr.y()*dt)*.95+thetaM*.05;
-    // phi=(phi-gyr.x()*dt)*.95+ phiM*.05;
-    // thetaG=thetaG+gyr.y()*dt;
-    // phiG=phiG-gyr.x()*dt;
-    
-    // phiRad=phi/360*(2*3.14);
-    // thetaRad=theta/360*(2*3.14);
-    
-    // Xm=mag.x()*cos(thetaRad)-mag.y()*sin(phiRad)*sin(thetaRad)+mag.z()*cos(phiRad)*sin(thetaRad);
-    // Ym=mag.y()*cos(phiRad)+mag.z()*sin(phiRad);
-    
-    // psi=atan2(Ym,Xm)/(2*3.14)*360;
-
-    // phiFold=phiFnew;
-    // thetaFold=thetaFnew;
-    // Serial.print(acc.x()/9.8);
-    // Serial.print(",");
-    // Serial.print(acc.y()/9.8);
-    // Serial.print(",");
-    // Serial.print(acc.z()/9.8);
-    // Serial.print(",");
-    // Serial.print(thetaM);
-    // Serial.print(",");
-    // Serial.print(phiM);
-    // Serial.print(",");
-    // Serial.print(thetaFnew);
-    // Serial.print(",");
-    // Serial.print(phiFnew);
-    // Serial.print(",");
-    // Serial.print(thetaG);
-    // Serial.print(",");
-    // Serial.print(phiG);
-    // Serial.print(",");
-    // Serial.print(theta);
-    // Serial.print(",");
-    // Serial.print(phi);
-    // Serial.print(",");
-    // Serial.println(psi);
 }
 
 void Plane::getData() {
     imu::Quaternion quat = bno.getQuat();
-    imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
 
-    bmp.getEvent(&event);
-    pressure = event.pressure;
-    bmp.getTemperature(&temperature);
-    altitude = bmp.pressureToAltitude(seaLevelPressure, pressure, temperature);
+    // bmp.getEvent(&event);
+    // pressure = event.pressure;
+    // bmp.getTemperature(&temperature);
+    // altitude = bmp.pressureToAltitude(seaLevelPressure, pressure, temperature);
 
-    q0 = quat.w();
-    q1 = quat.x();
-    q2 = quat.y();
-    q3 = quat.z();
+    q0=quat.w();
+    q1=quat.x();
+    q2=quat.y();
+    q3=quat.z();
 
     /* GET ROLL VALUES */
-    roll_current = atan2(2 * (q0 * q1 + q2 * q3), 1 - 2 * (q1 * q1 + q2 * q2));
-    roll_current = roll_current / (2 * PI) * 360;
+    roll_current = atan2(2*(q0*q1+q2*q3),1-2*(q1*q1+q2*q2));
+    roll_current = roll_current/(2*3.141592654)*360;
     roll_current = filter(roll_prev, roll_current);
     roll_prev = roll_current;
 
     /* GET PITCH VALUES */
-    pitch_current = asin(2 * (q0 * q2 - q3 * q1));
-    pitch_current = pitch_current / (2 * PI) * 360;
+    pitch_current = asin(2*(q0*q2-q3*q1));
+    pitch_current = pitch_current/(2*3.141592654)*360;
     pitch_current = filter(pitch_prev, pitch_current);
     pitch_prev = pitch_current;
 
@@ -124,12 +87,47 @@ void Plane::getData() {
     elapsed_time = (micros() - start_time) / 10000;
 }
 
-void Plane::getErrors() { 
-    roll_error = roll_current - roll_target;
+void Plane::getDesiredState() {
+    if (elapsed_time > 500 ) {
+        pitch_target = 15;
+        roll_target = 15;
+    }
+    if (elapsed_time > 1000 ) {
+        pitch_target = 30;
+        roll_target = 30;
+    }
+    if (elapsed_time > 1500 ) {
+        pitch_target = 15;
+        roll_target = 15;
+    }
+    if (elapsed_time > 2000 ) {
+        pitch_target = 0;
+        roll_target = 0;
+    }
+    if (elapsed_time > 2500 ) {
+        pitch_target = -15;
+        roll_target = -15;
+    }
+    if (elapsed_time > 3000 ) {
+        pitch_target = -30;
+        roll_target = -30;
+    }
+    if (elapsed_time > 3500 ) {
+        pitch_target = -15;
+        roll_target = -15;
+    }
+    if (elapsed_time > 4000 ) {
+        pitch_target = 0;
+        roll_target = 0;
+    }
+}
+
+void Plane::getErrors() {
+    roll_error = roll_target - roll_current;
     roll_error_base = roll_base - roll_current;
-    pitch_error = pitch_current - pitch_target; 
+    pitch_error = pitch_target - pitch_current; 
     pitch_error_base = pitch_base - pitch_current;
-    yaw_error = yaw_current - yaw_target;
+    yaw_error = yaw_target - yaw_current;
     yaw_error_base = yaw_base - yaw_current;
     heading_error = heading_current - heading_target;
     heading_error_base = (heading_base - heading_current) * -1;
@@ -141,18 +139,6 @@ void Plane::getPhase(uint8_t *phase_value) {
     *phase_value = flight_phase;
 };
 
-void Plane::getPressure() {
-    bmp.getEvent(&event);
-    pressure = event.pressure;
-}
-
-void Plane::getTemperature() { 
-    bmp.getTemperature(&temperature);
-    temperature = kalman(temperature);
-}
-
-void Plane::getAltitude() { altitude = bmp.pressureToAltitude(seaLevelPressure, pressure, temperature); }
-
 void Plane::printCurrentState() {
     Serial.print("Mission Time: ");
     Serial.println(elapsed_time);
@@ -163,31 +149,34 @@ void Plane::printCurrentState() {
     Serial.print(roll_error, 1);
     Serial.print("\tbase: ");
     Serial.print(roll_error_base, 1);
-    Serial.println("");
+    Serial.print("\ttarget: ");
+    Serial.print(roll_target);
+    Serial.print("\tVal: ");
+    Serial.print(roll_servo_val_l);
 
-    Serial.print("pitch: ");
-    Serial.print(pitch_current, 1);
-    Serial.print("\terror: ");
-    Serial.print(pitch_error, 1);
-    Serial.print("\tbase: ");
-    Serial.print(pitch_error_base, 1);
-    Serial.println("");
+    // Serial.print("pitch: ");
+    // Serial.print(pitch_current, 1);
+    // Serial.print("\terror: ");
+    // Serial.print(pitch_error, 1);
+    // Serial.print("\tbase: ");
+    // Serial.print(pitch_error_base, 1);
+    // Serial.println("");
 
-    Serial.print("yaw: ");
-    Serial.print(yaw_current, 1);
-    Serial.print("\terror: ");
-    Serial.print(yaw_error, 1);
-    Serial.print("\tbase: ");
-    Serial.print(yaw_error_base, 1);
-    Serial.println("");
+    // Serial.print("yaw: ");
+    // Serial.print(yaw_current, 1);
+    // Serial.print("\terror: ");
+    // Serial.print(yaw_error, 1);
+    // Serial.print("\tbase: ");
+    // Serial.print(yaw_error_base, 1);
+    // Serial.println("");
 
-    Serial.print("head: ");
-    Serial.print(heading_current, 0);
-    Serial.print("\terror: ");
-    Serial.print(heading_error, 0);
-    Serial.print("\tbase: ");
-    Serial.print(heading_error_base, 0);
-    Serial.println("");
+    // Serial.print("head: ");
+    // Serial.print(heading_current, 0);
+    // Serial.print("\terror: ");
+    // Serial.print(heading_error, 0);
+    // Serial.print("\tbase: ");
+    // Serial.print(heading_error_base, 0);
+    // Serial.println("");
 
     // Serial.print("pressure: ");
     // Serial.print(pressure);
@@ -199,7 +188,7 @@ void Plane::printCurrentState() {
     Serial.println("");
 }
 
-void Plane::calibratePlane() {
+bool Plane::calibratePlane() {
     uint8_t system, gyro, accel, mag;
     system = gyro = accel = mag = 0;
     int eeAddress = 0;
@@ -224,19 +213,31 @@ void Plane::calibratePlane() {
         bno.setSensorOffsets(calibrationData);
 
         Serial.println("\n\nCalibration data loaded into BNO055");
-        return;
+        return true;
     }
      
-    while(digitalRead(buttonApin) == HIGH) {
-        bno.getEvent(&event);
+    // while(digitalRead(buttonApin) == HIGH) {
+    //     bno.getEvent(&event);
+    //     bno.getCalibration(&system, &gyro, &accel, &mag);
+    //     Serial.print("X: ");
+    //     Serial.print(event.orientation.x, 4);
+    //     Serial.print("\tY: ");
+    //     Serial.print(event.orientation.y, 4);
+    //     Serial.print("\tZ: ");
+    //     Serial.print(event.orientation.z, 4);
+    //     Serial.print("\t\tSys:");
+    //     Serial.print(system, DEC);
+    //     Serial.print(" G:");
+    //     Serial.print(gyro, DEC);
+    //     Serial.print(" A:");
+    //     Serial.print(accel, DEC);
+    //     Serial.print(" M:");
+    //     Serial.println(mag, DEC);
+    //     delay(100);
+    // }
+   
         bno.getCalibration(&system, &gyro, &accel, &mag);
-        Serial.print("X: ");
-        Serial.print(event.orientation.x, 4);
-        Serial.print("\tY: ");
-        Serial.print(event.orientation.y, 4);
-        Serial.print("\tZ: ");
-        Serial.print(event.orientation.z, 4);
-        Serial.print("\t\tSys:");
+        Serial.print("Sys:");
         Serial.print(system, DEC);
         Serial.print(" G:");
         Serial.print(gyro, DEC);
@@ -244,22 +245,13 @@ void Plane::calibratePlane() {
         Serial.print(accel, DEC);
         Serial.print(" M:");
         Serial.println(mag, DEC);
-        delay(100);
-    }
+  
+        if(system == 3 && gyro == 3 && accel == 3 && mag == 3) {
+             bno.setSensorOffsets(calibrationData);
+             return true;
+        }
 
-        // while(!bno.isFullyCalibrated()) {
-            // bno.getCalibration(&system, &gyro, &accel, &mag);
-            // Serial.print("Sys:");
-            // Serial.print(system, DEC);
-            // Serial.print(" G:");
-            // Serial.print(gyro, DEC);
-            // Serial.print(" A:");
-            // Serial.print(accel, DEC);
-            // Serial.print(" M:");
-            // Serial.println(mag, DEC);
-        // }
- 
-        bno.setSensorOffsets(calibrationData);   
+        return false;
 }
 
 bool Plane::start(float pressure = SENSORS_PRESSURE_SEALEVELHPA) {
@@ -279,16 +271,18 @@ bool Plane::start(float pressure = SENSORS_PRESSURE_SEALEVELHPA) {
     
     bno.setExtCrystalUse(true);
     aileron_left.attach(3);
+    moveServo(&aileron_left, 135); 
 
-    // moveServo(&aileron_left, 0);
+    while(!calibratePlane()) {
+        Serial.print("Running");
+    }
 
-    pinMode(buttonApin, INPUT_PULLUP);  
-    calibratePlane();
-    
     // get raw data
     for (uint8_t i = 0; i < 250; i++) {
        getData();
     }
+
+    
 
     /* SET BASELINE NUMBERS */
     roll_base = roll_current;
